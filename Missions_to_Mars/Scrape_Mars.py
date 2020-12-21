6b4685d174138e7165456c123e2033395780623f
Desiree Herschberger <citizendez@gmail.com>
2020-12-21 15:14:48 -0500

    set up application framework

---
+++ Missions_to_Mars/Scrape_Mars.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+import pymongo
+from webdriver_manager.chrome import ChromeDriverManager
+from splinter import Browser
+import pandas as pd
+
+
+
+def scrape():
+    return 'Hello from scrape'
---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,18 @@
+# import necessary libraries
+from flask import Flask, render_template
+from Scrape_Mars import scrape
+
+# Initialize your Flask app here
+app = Flask(__name__)
+
+# Create a route, view function that takes in a list of strings and renders index.html template
+@app.route("/")
+def index():
+    mars_attacks = 'hello Q'
+    return render_template("index.html", thing=mars_attacks)
+
+@app.route("/scrape")
+def do_scape():
+    return  scrape()
+if __name__ == "__main__":
+    app.run(debug=True)
