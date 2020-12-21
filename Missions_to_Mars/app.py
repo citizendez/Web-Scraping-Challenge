4f829120d1a677419e326e92e760c03e7510f96c
Desiree Herschberger <citizendez@gmail.com>
2020-12-21 18:37:09 -0500

    create html

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,6 +1,7 @@
 # import necessary libraries
 from flask import Flask, render_template
 from Scrape_Mars import scrape
+import pymongo
 
 # Initialize your Flask app here
 app = Flask(__name__)
@@ -8,8 +9,15 @@ app = Flask(__name__)
 # Create a route, view function that takes in a list of strings and renders index.html template
 @app.route("/")
 def index():
-    mars_attacks = 'hello Q'
-    return render_template("index.html", thing=mars_attacks)
+    # Initialize PyMongo to work with MongoDBs
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+    # Define database and collection
+    db = client.mars_db
+    features = db.features.find()
+    feature = features[0]
+    return render_template('index.html', feature=feature)
+    
 
 @app.route("/scrape")
 def do_scape():
